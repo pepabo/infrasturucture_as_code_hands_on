a7ffbef253ee5c30484e2359173dd58241f657d3
pyama86 <pyama@pepabo.com>
2018-04-03 00:16:04 +0900

    check http response

--- spec/www/httpd_spec.rb
+++ spec/www/httpd_spec.rb
@@ -12,3 +12,7 @@ end
 describe port(80) do
   it { should be_listening }
 end
+
+describe command('curl http://127.0.0.1/wp-login.php -o /dev/null -w "%{http_code}\n" -s') do
+  its(:stdout) { should match /^200$/ }
+end
