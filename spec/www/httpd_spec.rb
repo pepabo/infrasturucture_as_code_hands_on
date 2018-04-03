441101ec3085671796432c14f0581f9a6414aaa1
pyama86 <pyama@pepabo.com>
2018-04-03 16:05:16 +0900

    wordpressやめる

--- spec/www/httpd_spec.rb
+++ spec/www/httpd_spec.rb
@@ -1,7 +1,12 @@
 require 'spec_helper'
-
-describe package('apache2') do
-  it { should be_installed }
+%w(
+  apache2
+  php7.0
+  libapache2-mod-php7.0
+).each do |n|
+  describe package(n) do
+    it { should be_installed }
+  end
 end
 
 describe service('apache2') do
@@ -13,6 +18,6 @@ describe port(80) do
   it { should be_listening }
 end
 
-describe command('curl http://127.0.0.1/wp-login.php -o /dev/null -w "%{http_code}\n" -s') do
+describe command('curl http://127.0.0.1/ -o /dev/null -w "%{http_code}\n" -s') do
   its(:stdout) { should match /^200$/ }
 end
--- spec/www/wordpress_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe file('/var/www/wordpress') do
-  it { should be_directory }
-  it { should be_mode 700 }
-  it { should be_owned_by 'apache' }
-  it { should be_grouped_into 'apache' }
-end
